91dd337cc45a2482cd05e1d142617466434fcb81
Aimable01 <aimable.kwizera14@gmail.com>
2025-12-16 10:39:53 +0200

    user entity

--- src/main/java/com/aimable01/blog/entity/User.java
+++ src/main/java/com/aimable01/blog/entity/User.java
@@ -1,4 +1,18 @@
 package com.aimable01.blog.entity;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
 }
