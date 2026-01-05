0b6985bd0c8edf984ab16ab72c07655c96255cea
Aimable01 <aimable.kwizera14@gmail.com>
2026-01-05 17:33:05 +0200

    feat: post repository

--- src/main/java/com/aimable01/blog/repository/PostRepository.java
+++ src/main/java/com/aimable01/blog/repository/PostRepository.java
@@ -1,4 +1,17 @@
 package com.aimable01.blog.repository;
 
-public class PostRepository {
+import com.aimable01.blog.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, String> {
+    Page<Post> findAllByOrderByCreatedAtAsc(Pageable pageable);
+
+    @Query("SELECT p FROM Post p WHERE P.author.id = :authorId " +
+            "ORDER BY p.createdAt DESC ")
+    Page<Post> findByAuthorId(String authorId, Pageable pageable);
 }
