d90e28540d53200309d727b76037f0a1f1107933
Aimable01 <aimable.kwizera14@gmail.com>
2026-01-05 17:20:58 +0200

    feat: comment response

--- src/main/java/com/aimable01/blog/dto/response/CommentResponse.java
+++ src/main/java/com/aimable01/blog/dto/response/CommentResponse.java
@@ -1,4 +1,15 @@
 package com.aimable01.blog.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class CommentResponse {
+    private String id;
+    private String content;
+    private String username;
+    private LocalDateTime createdAt;
 }
