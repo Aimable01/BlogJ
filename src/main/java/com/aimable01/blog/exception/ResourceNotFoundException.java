83408216addba3f384f710240a4c9e448efd57dc
Aimable01 <aimable.kwizera14@gmail.com>
2026-01-05 17:40:28 +0200

    resource not found exception

--- src/main/java/com/aimable01/blog/exception/ResourceNotFoundException.java
+++ src/main/java/com/aimable01/blog/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.aimable01.blog.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
