6d269f414f0a06611d3b4f2d33584dc152fddb1d
Aimable01 <aimable.kwizera14@gmail.com>
2026-01-05 17:18:53 +0200

    feat: user response dto

--- src/main/java/com/aimable01/blog/dto/response/UserResponse.java
+++ src/main/java/com/aimable01/blog/dto/response/UserResponse.java
@@ -1,4 +1,17 @@
 package com.aimable01.blog.dto.response;
 
+import com.aimable01.blog.enums.Role;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class UserResponse {
+    private String id;
+    private String username;
+    private String email;
+    private Role role;
+    private LocalDateTime createdAt;
 }
