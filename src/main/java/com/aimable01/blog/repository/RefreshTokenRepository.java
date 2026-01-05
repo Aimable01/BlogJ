b946084ed43e54d7739ce235ea48a0b44d8e6ee6
Aimable01 <aimable.kwizera14@gmail.com>
2026-01-05 17:39:29 +0200

    feat: refresh token repository

--- src/main/java/com/aimable01/blog/repository/RefreshTokenRepository.java
+++ src/main/java/com/aimable01/blog/repository/RefreshTokenRepository.java
@@ -1,4 +1,14 @@
 package com.aimable01.blog.repository;
 
-public class RefreshTokenRepository {
+import com.aimable01.blog.entity.RefreshToken;
+import com.aimable01.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByUser(User user);
 }
