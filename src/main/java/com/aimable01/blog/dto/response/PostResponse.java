1c99c7fe7cc9bbd90814c69f4e58257e15b917ce
Aimable01 <aimable.kwizera14@gmail.com>
2026-01-05 17:19:31 +0200

    feat: post response dto

--- src/main/java/com/aimable01/blog/dto/response/PostResponse.java
+++ src/main/java/com/aimable01/blog/dto/response/PostResponse.java
@@ -1,4 +1,20 @@
 package com.aimable01.blog.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
 public class PostResponse {
+    private String id;
+    private String title;
+    private String content;
+    private String authorUsername;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Integer commentCount;
+    private List<CommentResponse> comments;
 }
