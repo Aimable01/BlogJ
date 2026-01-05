a7317e576211e3fdfa806b604f91bc67ab6ade66
Aimable01 <aimable.kwizera14@gmail.com>
2026-01-05 17:19:24 +0200

    feat: auth response dto

--- src/main/java/com/aimable01/blog/dto/response/AuthResponse.java
+++ src/main/java/com/aimable01/blog/dto/response/AuthResponse.java
@@ -1,4 +1,19 @@
 package com.aimable01.blog.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+    private String userId;
+    private String username;
+    private String role;
 }
